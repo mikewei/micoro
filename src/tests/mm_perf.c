b0bc4fbf7a2aab9ebc923bb9dd0900b5c45864dc
Bin Wei <bin@vip.qq.com>
2012-06-10 19:25:58 +0800

    add USE_MALLOC

--- src/tests/mm_perf.c
+++ src/tests/mm_perf.c
@@ -1,10 +1,11 @@
 #include <time.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <assert.h>
 #include "coro_comm.h"
 #include "coro_mm.h"
 
-#define _assert
+//#define USE_MALLOC
 
 #define N (100000)
 static void *tab[N];
@@ -13,10 +14,16 @@ static size_t cur = 0;
 void prepare()
 {
 	size_t sz = 4096, i;
+#ifndef USE_MALLOC
 	assert(coro_mm_default_ops->init(&sz, N) == 0);
+#endif
 	assert(sz == 4096);
 	for (i=0; i<N; i++) {
+#ifdef USE_MALLOC
+		assert(tab[cur] = malloc(4096));	
+#else
 		assert(tab[i] = coro_mm_default_ops->alloc());
+#endif
 	}
 	srand(time(NULL));
 	cur = rand() % N;
@@ -24,7 +31,12 @@ void prepare()
 
 void testcase()
 {
+#ifdef USE_MALLOC
+	free(tab[cur]);
+	assert(tab[cur] = malloc(4096));	
+#else
 	assert(coro_mm_default_ops->release(tab[cur]) == 0);
 	assert(tab[cur] = coro_mm_default_ops->alloc());
+#endif
 	cur = (cur + 1) % N;
 }
